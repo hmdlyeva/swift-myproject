7b010ce0fbfca749bcb4d968d5b9f190279fc402
hmdlyeva <bd7fcqzu3@code.edu.az>
2024-11-11 02:13:00 +0400

    Initial Commit

---
+++ myproject/myprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  myprojectApp.swift
+//  myproject
+//
+//  Created by Hamida on 11.11.24.
+//
+
+import SwiftUI
+
+@main
+struct myprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ myprojectTests/myprojectTests.swift
@@ -0,0 +1,16 @@
+//
+//  myprojectTests.swift
+//  myprojectTests
+//
+//  Created by Hamida on 11.11.24.
+//
+
+import Testing
+
+struct myprojectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
